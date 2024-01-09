580265cff6c02da8f7d3d1456cb38c39c5bc66f3
AlexhsV <98179885+AlexhsV@users.noreply.github.com>
2024-01-09 20:43:03 +0200

    flask not working in brainwaveGraphs

--- flask-server/server.py
+++ flask-server/server.py
@@ -53,6 +53,21 @@ def schizophreniaHistory():
     return json.dumps(patient_documents_json)
 
 
+@app.route("/pulsegraph")
+def pulsegraph():
+    from flask import jsonify
+    hr_values, t_values = heart_rate_values()
+    print(hr_values)
+    print(type(hr_values))
+    print("test")
+    data = {
+        'hr_values': hr_values,
+        't_values': t_values
+    }
+    print(type(jsonify(data)))
+    return jsonify(data)  # convert document to json
+
+
 def predict(patient_name):
     import mne
     import numpy as np
@@ -303,6 +318,28 @@ def brainwaves():
     print(f"Deleted file: {edf_file_path}")
 
     return send_file(image_stream, mimetype='image/png')
+
+
+def heart_rate_values():
+    time_values = []
+    heart_rate_values = []
+
+    file_path = '../heart_rate/46343_heartrate.txt'
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            data = line.strip().split(',')  # data comma-separated
+            # time to float and strip the '-'
+            time_values.append(float(data[0].lstrip('-')))
+            # heart rate to int if needed
+            heart_rate_values.append(int(data[1]))
+
+    time_values.reverse()
+    heart_rate_values.reverse()
+    scale_factor = 10000
+    scaled_time_values = [t / scale_factor for t in time_values]
+
+    return time_values, scaled_time_values
 
 
 if __name__ == "__main__":
